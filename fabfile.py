83bdfead8a33e4723a3eaed733fb9090db535d79
Gavin Burris <gavin.idolinux@gmail.com>
2012-06-11 20:09:48 -0400

    included gpl license

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,20 @@
+# a fabfile for prototyping a grid engine cluster in amazon ec2
+#
+# Copyright (C) 2011-2012 Gavin Burris
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
